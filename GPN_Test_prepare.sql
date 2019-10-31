b3d1a5fa8c2cfa3a439d2e7d3765b5496d250836
Luxoft\Avasilevskii <avasilevskii@luxoft.com>
2019-10-31 14:03:35 +0300

    first commit

---
+++ GPN_Test_prepare.sql
@@ -0,0 +1,54 @@
+
+-- At the first step you need to create new clean database [GPN_Test] on your SQL Server instanse
+-- Next steps below:
+
+
+USE [GPN_Test]
+GO
+
+/* ---------------------------------------------- DenormalizedTable ---------------------------------------------- */
+
+-- Stage
+
+CREATE TABLE [stage].[DenormalizedTable]
+(
+	[SalesOrderID] [int] NULL,
+	[OrderQty] [smallint] NULL,
+	[ProductID] [int] NULL,
+	[UnitPrice] [money] NULL,
+	[Total] [money] NULL,
+	[OrderDate] [smalldatetime] NULL,
+	[ProductName] [nvarchar](50) NULL,
+	[ProductNumber] [nvarchar](25) NULL,
+	[ProductCategory] [nvarchar](50) NULL
+) ON [PRIMARY]
+
+-- Prod
+
+CREATE TABLE [dbo].[DenormalizedTable]
+(
+	[SalesOrderID] [int] NULL,
+	[OrderQty] [smallint] NULL,
+	[ProductID] [int] NULL,
+	[UnitPrice] [money] NULL,
+	[Total] [money] NULL,
+	[OrderDate] [smalldatetime] NULL,
+	[ProductName] [nvarchar](50) NULL,
+	[ProductNumber] [nvarchar](25) NULL,
+	[ProductCategory] [nvarchar](50) NULL
+) ON [PRIMARY]
+
+
+/* ---------------------------------------------- WeatherData ---------------------------------------------- */
+
+CREATE TABLE [dbo].[WeatherData]
+(
+	[name] [varchar](100) NULL,
+	[country] [varchar](100) NULL,
+	[sunrise] [bigint] NULL,
+	[sunset] [bigint] NULL,
+	[windSpeed] [decimal](35, 18) NULL,
+	[weatherDescription] [varchar](200) NULL,
+	[sunrise_date] [smalldatetime] NULL,
+	[sunset_date] [smalldatetime] NULL
+) ON [PRIMARY]
---
+++ readme.txt
@@ -0,0 +1,9 @@
+1) ETL_For_Test - etl solution was developed on VS 2015 Shell environment.
+Please use this VS version to open ETL - package inside "ETL_For_Test" folder
+
+2) I can't install ADventure Works 2017 on my server because my server version is 2016.
+Instead of this i have use ADwenture Works 2016 DB ( https://github.com/Microsoft/sql-server-samples/releases/tag/adventureworks )
+
+3) Before run ETL package from the first step you should create an empty database named "GPN_Test" and then run GPN_Test_prepare.sql - script.
+
+4) There is no implementation of the 4th task
